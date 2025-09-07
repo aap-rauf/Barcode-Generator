3991ce2a89c16a87d6f6947b47b0d457fca1de0c
Rauf <raufhossain548@gmail.com>
2025-09-07 17:46:06 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,37 @@
+const CACHE_NAME = "barcode-cache-v2"; // change version when you update
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./manifest.json",
+  "./icon-light-192.png",
+  "./icon-light-512.png"
+];
+
+// Install: cache files
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open("barcode-cache").then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./style.css",
-        "./manifest.json"
-      ]);
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
     })
   );
+  self.skipWaiting(); // activate worker immediately
 });
 
+// Activate: remove old caches
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// Fetch: serve cached files if offline
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(response => {
